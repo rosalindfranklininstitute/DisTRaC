73aa47ec493c642c62269ade7645e2d4402b02ec
= <=>
2022-05-18 08:17:48 +0100

    Passing filesystem agurgment
    This allows the user to specifiy the filesystem mount point.
    for creation or removal

--- distrac/distrac.sh
+++ distrac/distrac.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 rgw=false
-fs=false
 folder="."
-source ./helpmsg.sh
+source helpmsg.sh
 
 if [ -z "$@" ] 
 then 
@@ -16,6 +15,10 @@ case $i in
     -f=*|--folder=*)
     folder="${i#*=}"
     mkdir $folder 2> /dev/null
+    shift # past argument=value
+    ;;
+    -fs=*|--filesystem=*)
+    filesystem="${i#*=}"
     shift # past argument=value
     ;;
     -i=*|--interface=*)
@@ -40,10 +43,6 @@ case $i in
     ;;
     -rgw|--rgw)
     rgw=true
-    shift
-    ;;
-    -fs|--fs)
-    fs=true
     shift
     ;;
     -uid=*|--uid=*)
@@ -72,39 +71,44 @@ done
 echo $amount> $folder/amountOfOSDs.num
 if [ ! -z "$hostfile" ]
 then
-	source ./process-hostfile.sh -f=$folder -hf=$hostfile
+	source process-hostfile.sh -f=$folder -hf=$hostfile
 else
-	source ./uge-hostfile.sh -f=$folder
+	source uge-hostfile.sh -f=$folder
 fi
-
-./create-mon.sh -i=$interface -f=$folder
-./create-mgr.sh -f=$folder
+echo "Creating MON"
+create-mon.sh -i=$interface -f=$folder
+echo "Creating MGR"
+create-mgr.sh -f=$folder
 echo HOSTS:
 cat $folder/hostfile
 module load openmpi
 amountOfHosts=`cat $folder/amountOfHosts.num`
-mpirun -np $amountOfHosts --map-by ppr:1:node --hostfile $folder/hostfile  ./create-osd.sh -s=$size -n=$amount -f=$folder -t=$type
-
-if ([ "$fs" = "true" ])
-    then
-    ./create-mds.sh -f=$folder
-    mpirun -np $amountOfHosts --map-by ppr:1:node --hostfile $folder/hostfile  ./create-fs.sh
+echo "Creating OSDs"
+mpirun -np $amountOfHosts --map-by ppr:1:node --hostfile $folder/hostfile  create-osd.sh -s=$size -n=$amount -f=$folder -t=$type
+if ([ ! -z ${filesystem+x} ])
+then
+    echo "Creating MDS"
+    create-mds.sh -f=$folder
+    echo "Mounting Filesystem"
+    mpirun -np $amountOfHosts --map-by ppr:1:node --hostfile $folder/hostfile  create-fs.sh -fs=$filesystem
 fi
 
 if ([ ! -z "$poolname" ] && [ "$rgw" = "false" ])
 then
-   ./create-pool.sh -pn=$poolname -per=1 -f=$folder
+    echo "Creating POOL"
+    create-pool.sh -pn=$poolname -per=1 -f=$folder
 fi
 
 module unload openmpi
 
 if ([ "$rgw" = "true" ] && [ -z "$poolname" ])
 	then
-	./create-rgw.sh -f=$folder
+    echo "Creating RGW"
+	create-rgw.sh -f=$folder
 	if ([ -z "$id" ] && [ -z "$secret" ])
 	then
-		echo "No user will be create"
+		echo "No user will be created"
 	else
-		./create-s3-user.sh --uid=$id -sk=$secret
+		create-s3-user.sh --uid=$id -sk=$secret
 	fi
 fi
