7274bd8bc53e035ad811bdb0e263a6a8ec6a2fd7
= <=>
2022-05-18 08:11:47 +0100

    Allow user to specifiy filesystem location
    This now allows the user to mount a
    filesystem at anylocation they specifiy. Which means multiple
    DisTRaC instances could be mounted at one time.

--- distrac/create-fs.sh
+++ distrac/create-fs.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-sudo mkdir $HOME/mycephfs
-sudo mount -t ceph :/ $HOME/mycephfs -o name=admin,secret=`ceph auth print-key client.admin`
-sudo chown $USER:$USER $HOME/mycephfs
-sudo chmod 777 $HOME/mycephfs
+filesystem=""
+for i in "$@"
+do
+case $i in
+    -fs=*|--filesystem=*)
+    filesystem="${i#*=}"
+    mkdir $filesystem 2> /dev/null
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+sudo mount -t ceph :/ $filesystem -o name=admin,secret=`ceph auth print-key client.admin`
+sudo chown $USER:$USER $filesystem
+sudo chmod 777 $filesystem
--- distrac/remove-fs.sh
+++ distrac/remove-fs.sh
@@ -1,2 +1,18 @@
 #!/usr/bin/env bash
-sudo umount $HOME/mycephfs
+
+filesystem=""
+for i in "$@"
+do
+case $i in
+    -fs=*|--filesystem=*)
+    filesystem="${i#*=}"
+    mkdir $filesystem 2> /dev/null
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+sudo umount $filesystem
