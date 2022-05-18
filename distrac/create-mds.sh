7a2dd7b5e2c9b4e92879700d41d6564902ce0d64
= <=>
2022-05-18 08:23:11 +0100

    Removal of relative pathing and mkdir errors
    Relative pathing stops DisTRaC from being placed in /bin
    and mkdir errors do not need to be repeated.

--- distrac/create-mds.sh
+++ distrac/create-mds.sh
@@ -13,16 +13,15 @@ case $i in
     ;;
 esac
 done
-
 sudo mkdir /var/lib/ceph/mds/ceph-$HOSTNAME
 ceph auth get-or-create mds.$HOSTNAME osd "allow rwx" mds "allow" mon "allow profile mds" > $folder/ceph.mds.keyring
 chmod 644 $folder/ceph.mds.keyring
 sudo cp $folder/ceph.mds.keyring /var/lib/ceph/mds/ceph-$HOSTNAME/keyring
 sudo chown -R ceph:ceph  /var/lib/ceph/mds/ceph-$HOSTNAME/
 sudo systemctl start ceph-mds@$HOSTNAME
-./create-pool.sh -pn=cephfs_data -per=0.90 -f=$folder &
+create-pool.sh -pn=cephfs_data -per=0.90 -f=$folder &
 wait
-./create-pool.sh -pn=cephfs_metadata -per=0.10 -f=$folder &
+create-pool.sh -pn=cephfs_metadata -per=0.10 -f=$folder &
 wait 
 ceph fs new cephfs cephfs_metadata cephfs_data &
 wait 
