5287881ef8107e0fa1dfc251836df5f37c3cad32
= <gabryelmw@gmail.com>
2023-02-02 09:39:47 +0000

    Disable Ceph Services
    This stops the services from starting up again after reboot

--- distrac/remove-ceph.sh
+++ distrac/remove-ceph.sh
@@ -7,9 +7,15 @@ sudo systemctl stop ceph-mgr.target
 sudo systemctl stop ceph-mds.target
 sudo systemctl stop ceph-radosgw.target
 sudo systemctl stop ceph-crash.service
-sudo systemctl stop ceph-osd.target &
-osd=$!
-wait $osd
+sudo systemctl stop ceph-osd.target
+
+sudo systemctl disable ceph.target
+sudo systemctl disable ceph-mon.target
+sudo systemctl disable ceph-mgr.target
+sudo systemctl disable ceph-mds.target
+sudo systemctl disable ceph-radosgw.target
+sudo systemctl disable ceph-crash.service
+sudo systemctl disable ceph-osd.target 
 
 # Remove Ceph system
 sudo find "/var/lib/ceph/bootstrap-mgr/" -mindepth 1 -delete
@@ -33,3 +39,10 @@ sudo systemctl stop system-ceph\\x2dmds.slice
 sudo systemctl stop system-ceph\\x2dosd.slice 
 sudo systemctl stop system-ceph\\x2dradosgw.slice
 sudo systemctl stop system-ceph\\x2dvolume.slice
+
+sudo systemctl disable system-ceph\\x2dmgr.slice
+sudo systemctl disable system-ceph\\x2dmon.slice
+sudo systemctl disable system-ceph\\x2dmds.slice
+sudo systemctl disable system-ceph\\x2dosd.slice
+sudo systemctl disable system-ceph\\x2dradosgw.slice
+sudo systemctl disable system-ceph\\x2dvolume.slice
